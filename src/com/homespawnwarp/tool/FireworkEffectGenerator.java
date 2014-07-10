8427dc6fc3d410478cd3adad0c6ab5cbbba900a1
pieter3457 <pieteratmail@gmail.com>
2014-07-10 18:13:17 +0200

    upload new classes

---
+++ src/com/homespawnwarp/tool/FireworkEffectGenerator.java
@@ -0,0 +1,92 @@
+package com.homespawnwarp.tool;
+
+import java.util.Random;
+
+import org.bukkit.Color;
+import org.bukkit.FireworkEffect;
+import org.bukkit.FireworkEffect.Type;
+
+public class FireworkEffectGenerator {
+
+	private static final Random r = new Random();
+
+	public static FireworkEffect randomEffect() {
+		
+		int r1i = r.nextInt(17) + 1;
+		int r2i = r.nextInt(17) + 1;
+		Color c1 = getColor(r1i);
+		Color c2 = getColor(r2i);
+		
+		int rt = r.nextInt(4) + 1;
+		Type type = Type.BALL;
+		if (rt == 1)
+			type = Type.BALL;
+		if (rt == 2)
+			type = Type.BALL_LARGE;
+		if (rt == 3)
+			type = Type.BURST;
+		if (rt == 4)
+			type = Type.STAR;
+		
+		return FireworkEffect.builder()
+				.flicker(r.nextBoolean()).withColor(c1).withFade(c2)
+				.with(type).trail(r.nextBoolean()).build();
+	}
+
+	private static Color getColor(int i) {
+		Color c = null;
+		if (i == 1) {
+			c = Color.AQUA;
+		}
+		if (i == 2) {
+			c = Color.BLACK;
+		}
+		if (i == 3) {
+			c = Color.BLUE;
+		}
+		if (i == 4) {
+			c = Color.FUCHSIA;
+		}
+		if (i == 5) {
+			c = Color.GRAY;
+		}
+		if (i == 6) {
+			c = Color.GREEN;
+		}
+		if (i == 7) {
+			c = Color.LIME;
+		}
+		if (i == 8) {
+			c = Color.MAROON;
+		}
+		if (i == 9) {
+			c = Color.NAVY;
+		}
+		if (i == 10) {
+			c = Color.OLIVE;
+		}
+		if (i == 11) {
+			c = Color.ORANGE;
+		}
+		if (i == 12) {
+			c = Color.PURPLE;
+		}
+		if (i == 13) {
+			c = Color.RED;
+		}
+		if (i == 14) {
+			c = Color.SILVER;
+		}
+		if (i == 15) {
+			c = Color.TEAL;
+		}
+		if (i == 16) {
+			c = Color.WHITE;
+		}
+		if (i == 17) {
+			c = Color.YELLOW;
+		}
+
+		return c;
+	}
+}
